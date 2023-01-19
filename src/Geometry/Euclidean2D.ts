05d4142a9444b26220ee900d4a45fc977cc5b21d
Lance Pollard <lp@elk.fm>
2023-01-19 02:14:40 -0800

    cleanup euc2d

--- src/Geometry/Euclidean2D.ts
+++ src/Geometry/Euclidean2D.ts
@@ -61,7 +61,8 @@ export class Euclidean2D {
     v1.Normalize()
     let v2: Vector3D = p - lineP1
     let distanceAlongLine: number = v2.Dot(v1)
-    return lineP1 + v1 * distanceAlongLine
+
+    return lineP1.Add(v1.MultiplyWithNumber(distanceAlongLine))
   }
 
   static IntersectionLineLine(
@@ -72,8 +73,10 @@ export class Euclidean2D {
     /* out */ intersection: Vector3D,
   ): number {
     intersection = Vector3D.construct()
-    let n1: Vector3D = p2 - p1
-    let n2: Vector3D = p4 - p3
+
+    let n1: Vector3D = p2.Subtract(p1)
+    let n2: Vector3D = p4.Subtract(p3)
+
     //  Intersect?
     //  XXX - Handle the case where lines are one and the same separately?
     //          (infinite interesection points)
@@ -85,14 +88,16 @@ export class Euclidean2D {
     let d4: number = Euclidean2D.DistancePointLine(p4, p1, p2)
     //  Distances on the same side?
     //  This tripped me up.
-    let a3: number = Euclidean2D.AngleToClock(p3 - p1, n1)
-    let a4: number = Euclidean2D.AngleToClock(p4 - p1, n1)
+    let a3: number = Euclidean2D.AngleToClock(p3.Subtract(p1), n1)
+    let a4: number = Euclidean2D.AngleToClock(p4.Subtract(p1), n1)
 
     const sameSide = a3 > Math.PI ? a4 > Math.PI : a4 <= Math.PI
 
     const factor = sameSide ? d3 / (d3 - d4) : d3 / (d3 + d4)
-    intersection = p3 + n2 * factor
+    intersection = p3.Add(n2.MultiplyWithNumber(factor))
+
     //  XXX - Unfortunately, this is happening sometimes.
+
     if (
       !Tolerance.Zero(
         Euclidean2D.DistancePointLine(intersection, p1, p2),
@@ -104,23 +109,21 @@ export class Euclidean2D {
     return 1
   }
 
-  static IntersectionCircleCircle(
-    c1: Circle,
-    c2: Circle,
-    /* out */ p1: Vector3D,
-    /* out */ p2: Vector3D,
-  ): number {
-    p1 = Vector3D.construct()
-    p2 = Vector3D.construct()
+  static IntersectionCircleCircle(c1: Circle, c2: Circle): number {
+    let p1 = Vector3D.construct()
+    let p2 = Vector3D.construct()
+
     //  A useful page describing the cases in this function is:
     //  http://ozviz.wasp.uwa.edu.au/~pbourke/geometry/2circle/
     //  Maybe here now? http://paulbourke.net/geometry/circlesphere/
     //  Vector and distance between the centers.
-    let v: Vector3D = c2.Center - c1.Center
+    let v: Vector3D = c2.Center.Subtract(c1.Center)
     let d: number = v.Abs()
     let r1: number = c1.Radius
     let r2: number = c2.Radius
+
     //  Circle centers coincident.
+
     if (Tolerance.Zero(d)) {
       if (Tolerance.Equal(r1, r2)) {
         return -1
@@ -150,22 +153,27 @@ export class Euclidean2D {
       Tolerance.Equal(d, r1 + r2) ||
       Tolerance.Equal(d, Math.abs(r1 - r2))
     ) {
-      p1 = c1.Center + v * r1
+      p1 = c1.Center.Add(v.MultiplyWithNumber(r1))
       return 1
     }
 
     //  There must be two intersection points.
-    p2 = v * r1
-    p1 = v * r1
+    p2 = v.MultiplyWithNumber(r1)
+    p1 = v.MultiplyWithNumber(r1)
+
     let temp: number = (r1 * r1 - r2 * r2 + d * d) / (2 * d)
     let angle: number = Math.acos(temp / r1)
+
     console.assert(
       !Tolerance.Zero(angle) && !Tolerance.Equal(angle, Math.PI),
     )
+
     p1.RotateXY(angle)
     p2.RotateXY(angle * -1)
-    p1 = p1 + c1.Center
-    p2 = p2 + c1.Center
+
+    p1 = p1.Add(c1.Center)
+    p2 = p2.Add(c1.Center)
+
     return 2
   }
 
@@ -178,12 +186,14 @@ export class Euclidean2D {
   ): number {
     p1 = Vector3D.construct()
     p2 = Vector3D.construct()
+
     //  Distance from the circle center to the closest point on the line.
     let d: number = Euclidean2D.DistancePointLine(
       circle.Center,
       lineP1,
       lineP2,
     )
+
     //  No intersection points.
     let r: number = circle.Radius
     if (d > r) {
@@ -192,6 +202,7 @@ export class Euclidean2D {
 
     //  One intersection point.
     p1 = Euclidean2D.ProjectOntoLine(circle.Center, lineP1, lineP2)
+
     if (Tolerance.Equal(d, r)) {
       return 1
     }
@@ -204,23 +215,28 @@ export class Euclidean2D {
     //  The line was close to going through the center and the second block was used,
     //  so I had to loosen the tolerance used by my comparison macros.
     if (Tolerance.Zero(d)) {
-      let line: Vector3D = lineP2 - lineP1
+      let line: Vector3D = lineP2.Subtract(lineP1)
       line.Normalize()
-      line = line * r
-      p1 = circle.Center + line
-      p2 = circle.Center - line
+
+      line = line.MultiplyWithNumber(r)
+
+      p1 = circle.Center.Add(line)
+      p2 = circle.Center.Subtract(line)
     } else {
       //  To origin.
-      p1 = p1 - circle.Center
+      p1 = p1.Subtract(circle.Center)
       p1.Normalize()
-      p1 = p1 * r
+
+      p1 = p1.MultiplyWithNumber(r)
       p2 = p1
+
       let angle: number = Math.acos(d / r)
       p1.RotateXY(angle)
       p2.RotateXY(angle * -1)
+
       //  Back out.
-      p1 = p1 + circle.Center
-      p2 = p2 + circle.Center
+      p1 = p1.Add(circle.Center)
+      p2 = p2.Add(circle.Center)
     }
 
     return 2
@@ -235,7 +251,8 @@ export class Euclidean2D {
     p2: Vector3D,
   ): Vector3D {
     let p: Vector3D = Euclidean2D.ProjectOntoLine(input, p1, p2)
-    return input + (p - input) * 2
+
+    return input.Add(p.Subtract(input).MultiplyWithNumber(2))
   }
 
   static SameSideOfLine(
@@ -244,12 +261,15 @@ export class Euclidean2D {
     test1: Vector3D,
     test2: Vector3D,
   ): boolean {
-    let d: Vector3D = lineP2 - lineP1
-    let t1: Vector3D = (test1 - lineP1).Cross(d)
-    let t2: Vector3D = (test2 - lineP1).Cross(d)
+    let d: Vector3D = lineP2.Subtract(lineP1)
+    let t1: Vector3D = test1.Subtract(lineP1).Cross(d)
+    let t2: Vector3D = test2.Subtract(lineP1).Cross(d)
+
     let pos1: boolean = t1.Z > 0
     let pos2: boolean = t2.Z > 0
-    return !(pos1 | pos2)
+
+    // MMM: Changed from | to ||
+    return !(pos1 || pos2)
     // The operator should be an XOR ^ instead of an OR, but not available in CodeDOM
   }
 }
